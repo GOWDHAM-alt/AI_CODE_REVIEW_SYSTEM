a0ce93a8e3bde38f1484e44aa68ef18fb2eb2dda
GOWDHAM-alt <gowdhamrathinasamy@gmail.com>
2026-03-17 10:12:45 +0530

    Update axios.js
    updated api for backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://ai-code-review-system-djup.onrender.com",
 });
 
 // Add token to every request
